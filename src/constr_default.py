313acbb04ae0ab5a6b0ed008adf9198dafd01fde
L. Linkous <LC-Linkous@users.noreply.github.com>
2024-05-20 19:51:12 -0400

    upload of src project files

---
+++ src/constr_default.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+
+##--------------------------------------------------------------------\
+#   pso_basic
+#   './pso_basic/src/constr_default.py'
+#   Function for default constraints. Called if user does not pass in 
+#       constraints for objective function or problem being optimized. 
+#
+#   Author(s): Jonathan Lundquist, Lauren Linkous
+#   Last update: May 20, 2024
+##--------------------------------------------------------------------\
+
+
+import numpy as np
+
+def constr_default(x):
+    return True
