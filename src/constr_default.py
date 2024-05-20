d9ad468ff6436fda0aa330f04331942761c1c0d7
L. Linkous <LC-Linkous@users.noreply.github.com>
2024-05-20 19:39:55 -0400

    Update constr_default.py

--- src/constr_default.py
+++ src/constr_default.py
@@ -1,13 +1,13 @@
 #! /usr/bin/python3
 
 ##--------------------------------------------------------------------\
-#   pso_python
-#   './src/pso_python/constr_default.py'
+#   pso_basic
+#   './src/pso_basic/constr_default.py'
 #   Function for default constraints. Called if user does not pass in 
 #       constraints for objective function or problem being optimized. 
 #
 #   Author(s): Jonathan Lundquist, Lauren Linkous
-#   Last update: May 4, 2024
+#   Last update: May 20, 2024
 ##--------------------------------------------------------------------\
 
 
