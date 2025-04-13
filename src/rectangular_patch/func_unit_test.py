4c8092062eded0c7ffeab3eb76c19cbb4d956bf9
LC_Linkous <L.C.Linkous@gmail.com>
2025-04-13 19:42:01 -0400

    rectangular patch ex eqs

---
+++ src/rectangular_patch/func_unit_test.py
@@ -0,0 +1,61 @@
+#! /usr/bin/python3
+
+##--------------------------------------------------------------------\
+#   pso_basic
+#   '.src/rectangular_patch/func_unit_test.py'
+#   Unit test for the objective function. Replaces graph.py for preview
+#
+#
+#   Author(s): Lauren Linkous
+#   Last update: April 13, 2025
+##-------------------------------------------------------------------------------\
+import numpy as np
+
+def func_F(X, NO_OF_OUTS=1):
+
+    F = np.zeros((NO_OF_OUTS))
+    noErrors = True
+
+    # constants
+    ## speed of light in m/s
+    c = 3e8
+    ## substrate information
+    h = 1.6  # standard thickness of FR4
+    epsilon_r = 4.4 # dielectric constant. standard permitivity of FR4
+
+
+    try:
+
+        length =  X[0]  # length in mm 
+        width = X[1]    # width in mm
+
+        # convert dimensions from mm to m
+        length = length / 1000
+        width = width / 1000
+        h = h / 1000
+
+        # calculate effective dielectric constant
+        epsilon_eff = ((epsilon_r + 1) / 2) + ((epsilon_r - 1) / 2) * (1 + 12 * h / width) ** (-0.5)
+        
+        # calculate extension length
+        delta_L = 0.412 * h * ((epsilon_eff + 0.3) * (width / h + 0.264)) / ((epsilon_eff - 0.258) * (width / h + 0.8))
+        
+        # calculate effective length
+        L_eff = length + 2 * delta_L
+        
+        # calculate resonant frequency (in Hz)
+        F[0] = c / (2 * L_eff * (epsilon_eff ** 0.5))
+        
+    except:
+        noErrors = False
+    
+    return F, noErrors
+
+# test
+length = 29.42  #mm
+width =  38.01  #mm
+
+f, noErr =  func_F([length, width])
+
+print(f)
+
